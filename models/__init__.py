26a04655745f485f37c2fe32b4e60d9e8b962b3b
levrekhlov <rekhlov.lev@mail.ru>
2023-12-16 02:29:15 +0300

    Add CNN Model (without testing).

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
-from models.linear_model import LinearModel
 from models.base_model import BaseModel
+from models.linear_model import LinearModel
+from models.cnn_model import CNNModel
 
 all_models = {
     "LinearModel": LinearModel
--- models/cnn_model.py
+++ models/cnn_model.py
@@ -1,10 +1,11 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
 
 from models.base_model import BaseModel
 
 
-class LinearModel(BaseModel):
+class CNNModel(BaseModel):
     def __init__(self, input_dim, hidden_dim, output_dim,
                  optimizer_func=None, hidden_layers_num=1,
                  apply_softmax=False):
@@ -21,26 +22,25 @@ class LinearModel(BaseModel):
             print(f"Hidden layers num {self.hidden_layers_num} is less than 0. Resetting to 0...")
             self.hidden_layers_num = 0
 
-        self.input_layer = nn.Linear(self.input_dim, self.hidden_dim)
+        self.input_layer = nn.Conv2d(in_channels=1, out_channels=self.hidden_dim[0], kernel_size=5)
 
-        self.hidden_layers = nn.Sequential(
-            *[
-                nn.Linear(self.hidden_dim, self.hidden_dim)
-                for _ in range(self.hidden_layers_num)
-            ]
-        ) if self.hidden_layers_num > 0 else None
+        self.hidden_layers = [
+            nn.Conv2d(in_channels=self.hidden_dim[i + 1], out_channels=self.hidden_dim[i + 2], kernel_size=5)
+            for i in range(self.hidden_layers)
+        ] if self.hidden_layers_num > 0 else None
 
-        self.output_layer = nn.Linear(self.hidden_dim, self.output_dim)
+        self.output_layer = nn.Linear(512, self.output_dim)
 
         self.softmax = nn.Softmax() if self.apply_softmax else None
 
     def forward(self, x):
-        x = x.view(x.shape[0], -1)
-
-        x = F.relu(self.input_layer(x))
+        x = F.max_pool2d(self.input_layer(x), kernel_size=2)
 
         if self.hidden_layers:
-            x = F.relu(self.hidden_layers(x))
+            for layer in self.hidden_layers:
+                x = F.max_pool2d(layer(x), kernel_size=2)
+
+        x = x.view(x.shape[0], -1)
 
         x = self.output_layer(x)
 
